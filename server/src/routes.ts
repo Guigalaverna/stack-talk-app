bb1593b4f11f32bac7ac0f0195b46a3ae663fab6
Guilherme Galaverna <guigalaverna@gmail.com>
2021-05-07 02:34:57 -0300

    Create new routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
+import RSSController from './controllers/RSSController'
 
 const router = Router()
 
-router.get('/episodes')
+router.get('/episodes', RSSController.index)
 router.get('/episodes/:id')
 
 export default router
